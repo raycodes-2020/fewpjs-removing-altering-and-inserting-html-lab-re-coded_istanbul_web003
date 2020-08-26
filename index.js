764b42b8e44a970d32b0ec7aab2ac12aa0fa2537
ray <rayissam22@gmail.com>
2020-08-26 21:10:59 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Ray is the champion";
